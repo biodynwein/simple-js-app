6fca2689456ddc8df9b01d136d35a66859b9961c
biodynwein <96068785+biodynwein@users.noreply.github.com>
2022-03-01 15:30:10 +0100

    JS update with pokemons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
-alert('Hello world');
+//list of pokemon
+let pokemonList = [{
+    name: 'Balbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass', 'poison']
+  },
 
-var favoriteFood = "Pizza";
-document.write(favoriteFood);
+  {
+    name: 'Charmilion',
+    height: 1.1,
+    weight: 19,
+    type: ['fire']
+  },
+
+  {
+    name: 'Dratini',
+    height: 1.8,
+    weight: 3.3,
+    type: ['dragpn']
+  },
+
+  {
+    name: 'Rayquaza',
+    height: 7,
+    weight: 206.5,
+    type: ['dragon']
+  }
+  ];
+  
+  //loops through list
+
+  for (let i = 0; i < pokemonList.length; i++) {
+     document.write(pokemonList[i].name + " height: "+ pokemonList[i].height + ", ");
+
+  //checks height, if more than 2 it writes "...big!"
+
+    if (pokemonList[i].height >2) {
+      document.write(" - Wow that's big!");
+   }
+  };
