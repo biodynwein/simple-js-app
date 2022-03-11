8a1902e24d3709d704367965a25bed2785c31bb7
biodynwein <96068785+biodynwein@users.noreply.github.com>
2022-03-11 10:41:00 +0100

    update  js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,53 @@
-//list of pokemon
-let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    weight: 6.9,
-    type: ['grass', 'poison']
-  },
+// IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [{
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison']
+},
 
-  {
-    name: 'Charmilion',
-    height: 1.1,
-    weight: 19,
-    type: ['fire']
-  },
+{
+  name: 'Charmilion',
+  height: 1.1,
+  weight: 19,
+  type: ['fire']
+},
 
-  {
-    name: 'Dratini',
-    height: 1.8,
-    weight: 3.3,
-    type: ['dragpn']
-  },
+{
+  name: 'Dratini',
+  height: 1.8,
+  weight: 3.3,
+  type: ['dragpn']
+},
 
-  {
-    name: 'Rayquaza',
-    height: 7,
-    weight: 206.5,
-    type: ['dragon']
+{
+  name: 'Rayquaza',
+  height: 7,
+  weight: 206.5,
+  type: ['dragon']
+},
+];
+// Public functions
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-  ];
 
-  //loops through list
+  function getAll() {
+    return pokemonList;
+  }
 
-  for (let i = 0; i < pokemonList.length; i++) {
-     document.write(pokemonList[i].name + " height: "+ pokemonList[i].height + ", ");
-
-  //checks height, if more than 2 it writes "...big!"
-
-    if (pokemonList[i].height >2) {
-      document.write(" - Wow that's big!");
-   }
+  return {
+    add: add,
+    getAll: getAll
   };
+})();
+// Updated forEach loop
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(pokemon.name + " height: "+ pokemon.height + ", ");
+  document.write ("<br/>");
+});
+// Add object
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Raichu' });
+console.log(pokemonRepository.getAll());
